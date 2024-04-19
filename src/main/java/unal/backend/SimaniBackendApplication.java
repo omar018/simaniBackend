256634a0df52f54d5831a5e92eadac1b2e63cb3c
omar018 <ochavez@unal.edu.co>
2024-04-19 10:49:45 -0500

    first commit

---
+++ src/main/java/unal/backend/SimaniBackendApplication.java
@@ -0,0 +1,13 @@
+package unal.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimaniBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimaniBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/unal/backend/SimaniBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package unal.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimaniBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
